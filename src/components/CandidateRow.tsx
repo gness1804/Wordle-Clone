4af0968192ee88bc8ea0e481c62ccb6a0ae39a10
gness1804 <graham.nessler@gmail.com>
2022-02-12 16:04:46 -0600

    automatically go to next input when entering letters

--- src/components/CandidateRow.tsx
+++ src/components/CandidateRow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { TextInput, View } from 'react-native';
 import tailwind from 'tailwind-rn';
 
@@ -8,6 +8,12 @@ interface Props {
 }
 
 const CandidateRow = ({ targetWord, disabled = true }: Props) => {
+  const firstLetterBox = useRef(null);
+  const secondLetterBox = useRef(null);
+  const thirdLetterBox = useRef(null);
+  const fourthLetterBox = useRef(null);
+  const fifthLetterBox = useRef(null);
+
   const [word, setWord] = useState<string>('');
   const [firstLetter, setFirstLetter] = useState<string>('');
   const [secondLetter, setSecondLetter] = useState<string>('');
@@ -15,11 +21,37 @@ const CandidateRow = ({ targetWord, disabled = true }: Props) => {
   const [fourthLetter, setFourthLetter] = useState<string>('');
   const [fifthLetter, setFifthLetter] = useState<string>('');
 
+  // go to the next input box on text input (if there is text)
+  useEffect(() => {
+    if (!firstLetter) return;
+    secondLetterBox.current?.focus();
+  }, [firstLetter]);
+
+  useEffect(() => {
+    console.log('secondLetter:', secondLetter);
+    if (!secondLetter) return;
+    thirdLetterBox.current?.focus();
+  }, [secondLetter]);
+
+  useEffect(() => {
+    if (!thirdLetter) return;
+    fourthLetterBox.current?.focus();
+  }, [thirdLetter]);
+
+  useEffect(() => {
+    if (!fourthLetter) return;
+    fifthLetterBox.current?.focus();
+  }, [fourthLetter]);
+
   return (
     <View style={tailwind('flex flex-row mb-2')}>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
         <TextInput
-          style={tailwind(`w-full h-full ${disabled && !firstLetter ? 'bg-gray-400' : ''}`)}
+          ref={firstLetterBox}
+          maxLength={1}
+          style={tailwind(
+            `w-full h-full ${disabled && !firstLetter ? 'bg-gray-400' : ''}`
+          )}
           onChangeText={(newText) => setFirstLetter(newText)}
           editable={!disabled}
           selectTextOnFocus={!disabled}
@@ -27,7 +59,11 @@ const CandidateRow = ({ targetWord, disabled = true }: Props) => {
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
         <TextInput
-          style={tailwind(`w-full h-full ${disabled && !secondLetter ? 'bg-gray-400' : ''}`)}
+          ref={secondLetterBox}
+          maxLength={1}
+          style={tailwind(
+            `w-full h-full ${disabled && !secondLetter ? 'bg-gray-400' : ''}`
+          )}
           onChangeText={(newText) => setSecondLetter(newText)}
           editable={!disabled}
           selectTextOnFocus={!disabled}
@@ -35,7 +71,11 @@ const CandidateRow = ({ targetWord, disabled = true }: Props) => {
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
         <TextInput
-          style={tailwind(`w-full h-full ${disabled && !thirdLetter ? 'bg-gray-400' : ''}`)}
+          ref={thirdLetterBox}
+          maxLength={1}
+          style={tailwind(
+            `w-full h-full ${disabled && !thirdLetter ? 'bg-gray-400' : ''}`
+          )}
           onChangeText={(newText) => setThirdLetter(newText)}
           editable={!disabled}
           selectTextOnFocus={!disabled}
@@ -43,7 +83,11 @@ const CandidateRow = ({ targetWord, disabled = true }: Props) => {
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
         <TextInput
-          style={tailwind(`w-full h-full ${disabled && !fourthLetter ? 'bg-gray-400' : ''}`)}
+          ref={fourthLetterBox}
+          maxLength={1}
+          style={tailwind(
+            `w-full h-full ${disabled && !fourthLetter ? 'bg-gray-400' : ''}`
+          )}
           onChangeText={(newText) => setFourthLetter(newText)}
           editable={!disabled}
           selectTextOnFocus={!disabled}
@@ -51,7 +95,11 @@ const CandidateRow = ({ targetWord, disabled = true }: Props) => {
       </View>
       <View style={tailwind('border-2 border-gray-300 h-10 w-10')}>
         <TextInput
-          style={tailwind(`w-full h-full ${disabled && !fifthLetter ? 'bg-gray-400' : ''}`)}
+          ref={fifthLetterBox}
+          maxLength={1}
+          style={tailwind(
+            `w-full h-full ${disabled && !fifthLetter ? 'bg-gray-400' : ''}`
+          )}
           onChangeText={(newText) => setFifthLetter(newText)}
           editable={!disabled}
           selectTextOnFocus={!disabled}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import tailwind from 'tailwind-rn';
 import CandidateRow from './CandidateRow';
 
@@ -7,32 +7,40 @@ const Main = () => {
   const [activeRow, setActiveRow] = useState<number>(1);
 
   const targetWord = 'apple'; // TODO: make dynamic
+
+  const handleSubmit = () => {
+
+  };
+
   return (
     <View style={tailwind('items-center justify-center')}>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 1}
-      ></CandidateRow>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 2}
-      ></CandidateRow>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 3}
-      ></CandidateRow>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 4}
-      ></CandidateRow>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 5}
-      ></CandidateRow>
-      <CandidateRow
-        targetWord={targetWord}
-        disabled={activeRow !== 6}
-      ></CandidateRow>
+      <View style={tailwind('mb-10')}>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 1}
+        ></CandidateRow>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 2}
+        ></CandidateRow>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 3}
+        ></CandidateRow>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 4}
+        ></CandidateRow>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 5}
+        ></CandidateRow>
+        <CandidateRow
+          targetWord={targetWord}
+          disabled={activeRow !== 6}
+        ></CandidateRow>
+      </View>
+      <Button onPress={handleSubmit} title='Submit' />
     </View>
   );
 };
