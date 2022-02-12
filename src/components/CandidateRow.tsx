5b768f491c8210150bf37fa654bbe3d70aeedcbf
gness1804 <graham.nessler@gmail.com>
2022-02-12 15:44:39 -0600

    build out rows

--- src/components/CandidateRow.tsx
+++ src/components/CandidateRow.tsx
@@ -2,25 +2,60 @@ import React, { useState } from 'react';
 import { TextInput, View } from 'react-native';
 import tailwind from 'tailwind-rn';
 
-const CandidateRow = () => {
-  const [word, setWord] = useState('');
+interface Props {
+  targetWord: string;
+  disabled: boolean;
+}
+
+const CandidateRow = ({ targetWord, disabled = true }: Props) => {
+  const [word, setWord] = useState<string>('');
+  const [firstLetter, setFirstLetter] = useState<string>('');
+  const [secondLetter, setSecondLetter] = useState<string>('');
+  const [thirdLetter, setThirdLetter] = useState<string>('');
+  const [fourthLetter, setFourthLetter] = useState<string>('');
+  const [fifthLetter, setFifthLetter] = useState<string>('');
 
   return (
     <View style={tailwind('flex flex-row mb-2')}>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
-        <TextInput style={tailwind('w-full h-full')} />
+        <TextInput
+          style={tailwind(`w-full h-full ${disabled && !firstLetter ? 'bg-gray-400' : ''}`)}
+          onChangeText={(newText) => setFirstLetter(newText)}
+          editable={!disabled}
+          selectTextOnFocus={!disabled}
+        />
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
-        <TextInput style={tailwind('w-full h-full')} />
+        <TextInput
+          style={tailwind(`w-full h-full ${disabled && !secondLetter ? 'bg-gray-400' : ''}`)}
+          onChangeText={(newText) => setSecondLetter(newText)}
+          editable={!disabled}
+          selectTextOnFocus={!disabled}
+        />
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
-        <TextInput style={tailwind('w-full h-full')} />
+        <TextInput
+          style={tailwind(`w-full h-full ${disabled && !thirdLetter ? 'bg-gray-400' : ''}`)}
+          onChangeText={(newText) => setThirdLetter(newText)}
+          editable={!disabled}
+          selectTextOnFocus={!disabled}
+        />
       </View>
       <View style={tailwind('mr-2 border-2 border-gray-300 h-10 w-10')}>
-        <TextInput style={tailwind('w-full h-full')} />
+        <TextInput
+          style={tailwind(`w-full h-full ${disabled && !fourthLetter ? 'bg-gray-400' : ''}`)}
+          onChangeText={(newText) => setFourthLetter(newText)}
+          editable={!disabled}
+          selectTextOnFocus={!disabled}
+        />
       </View>
       <View style={tailwind('border-2 border-gray-300 h-10 w-10')}>
-        <TextInput style={tailwind('w-full h-full')} />
+        <TextInput
+          style={tailwind(`w-full h-full ${disabled && !fifthLetter ? 'bg-gray-400' : ''}`)}
+          onChangeText={(newText) => setFifthLetter(newText)}
+          editable={!disabled}
+          selectTextOnFocus={!disabled}
+        />
       </View>
     </View>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import tailwind from 'tailwind-rn';
 import CandidateRow from './CandidateRow';
 
 const Main = () => {
+  const [activeRow, setActiveRow] = useState<number>(1);
+
+  const targetWord = 'apple'; // TODO: make dynamic
   return (
     <View style={tailwind('items-center justify-center')}>
-      <CandidateRow></CandidateRow>
-      <CandidateRow></CandidateRow>
-      <CandidateRow></CandidateRow>
-      <CandidateRow></CandidateRow>
-      <CandidateRow></CandidateRow>
-      <CandidateRow></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 1}
+      ></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 2}
+      ></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 3}
+      ></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 4}
+      ></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 5}
+      ></CandidateRow>
+      <CandidateRow
+        targetWord={targetWord}
+        disabled={activeRow !== 6}
+      ></CandidateRow>
     </View>
   );
 };
